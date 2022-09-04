126e3fa57b1355db9ce322eb87ee9cf140913b5c
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-09-04 17:16:29 +0500

    Add files via upload

---
+++ umodx/compat/geek.py
@@ -0,0 +1,47 @@
+#            ▀█▀ █ █  █▀█  █▀▄▀█ ▄▀█  █▀
+#             █  █▀█ █▄█ █ ▀ █ █▀█ ▄█  
+#             https://t.me/netuzb
+#
+# 🔒 Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+import re
+
+
+def compat(code: str) -> str:
+    """Reformats modules, built for GeekTG to work with umodx"""
+    return "\n".join(
+        [
+            re.sub(
+                r"^( *)from \.\.inline import (.+)$",
+                r"\1from ..inline.types import \2",
+                re.sub(
+                    r"^( *)from \.\.inline import rand[^,]*$",
+                    "\1from ..utils import rand",
+                    re.sub(
+                        r"^( *)from \.\.inline import rand, ?(.+)$",
+                        r"\1from ..inline.types import \2\n\1from ..utils import rand",
+                        re.sub(
+                            r"^( *)from \.\.inline import (.+), ?rand[^,]*$",
+                            r"\1from ..inline.types import \2\n\1from ..utils import"
+                            r" rand",
+                            re.sub(
+                                r"^( *)from \.\.inline import (.+), ?rand, ?(.+)$",
+                                r"\1from ..inline.types import \2, \3\n\1from ..utils"
+                                r" import rand",
+                                line.replace("GeekInlineQuery", "InlineQuery").replace(
+                                    "self.inline._bot", "self.inline.bot"
+                                ),
+                                flags=re.M,
+                            ),
+                            flags=re.M,
+                        ),
+                        flags=re.M,
+                    ),
+                    flags=re.M,
+                ),
+                flags=re.M,
+            )
+            for line in code.splitlines()
+        ]
+    )
