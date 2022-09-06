e2b8cbf9e9976603ea3eccafeaa58fbf34781b9e
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-09-06 23:52:45 +0500

    Update umodx_settings.py

--- umodx/modules/umodx_settings.py
+++ umodx/modules/umodx_settings.py
@@ -169,6 +169,52 @@ class SettingsMod(loader.Module):
             if watcher.__self__.__class__.strings is not None
         ], self._db.get(main.__name__, "disabled_watchers", {})
 
+    async def purgecmd(self, message):
+        """Purge from the replied message"""
+        if not message.is_reply:
+            await utils.answer(message, self.strings("from_where", message))
+            return
+        from_users = set()
+        args = utils.get_args(message)
+        for arg in args:
+            try:
+                try:
+                    arg = int(arg)
+                except:
+                    pass
+                entity = await message.client.get_entity(arg)
+                if isinstance(entity, telethon.tl.types.User):
+                    from_users.add(entity.id)
+            except ValueError:
+                pass
+        msgs = []
+        from_ids = set()
+        if await message.client.is_bot():
+            if not message.is_channel:
+                await utils.answer(message, self.strings("not_supergroup_bot", message))
+                return
+            for msg in range(message.reply_to_msg_id, message.id + 1):
+                msgs.append(msg)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        else:
+            async for msg in message.client.iter_messages(
+                entity=message.to_id, min_id=message.reply_to_msg_id - 1, reverse=True
+            ):
+                if from_users and msg.sender_id not in from_users:
+                    continue
+                msgs.append(msg.id)
+                from_ids.add(msg.sender_id)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        if msgs:
+            logger.debug(msgs)
+            await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("purge", group=message.to_id, affected_uids=from_ids)
 
     @loader.command(ru_doc="Очистить логи")
     async def clearlogs(self, message: Message):
